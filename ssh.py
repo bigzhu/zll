27b7490008ad808f330c4035d66961e8042b4d76
bigzhu <vermiliondun@gmail.com>
2016-03-24 10:43:45 +0800

    重构了一下，支持port

--- ssh.py
+++ ssh.py
@@ -1,81 +1,75 @@
 #!/usr/bin/python
-#encoding=utf-8
-#file_name=ssh.py
+# encoding=utf-8
+# file_name=ssh.py
 
 import sys
 import os
 
 
-def init_ssh_info():
+def readConf():
+    '''
+    读取置文件
+    '''
     ssh_info = []
     f = open(sys.path[0] + '/ssh.ini', 'r')
     for i in f:
         ssh_one_info = i.split()
-
         if(len(ssh_one_info) != 0):
             ssh_info.append(tuple(i.split()))
     f.close()
     return sorted(ssh_info, key=lambda by: by[1])
 
 
-def ssh(user, ip, passwd):
-    command = "export TERM=xterm;ssh %s@%s" % (user, ip)
+def ssh(ssh_info):
+    user = ssh_info[0]
+    ip = ssh_info[1]
+    port = 22
+    if len(ssh_info) > 4:
+        port = ssh_info[4]
+    command = "export TERM=xterm;ssh -p %s %s@%s" % (port, user, ip)
     os.system(command)
 
 
-def print_info(ssh_info):
+def printInfo(ssh_info):
     for i in ssh_info:
-        print str(ssh_info.index(i)).ljust(4), i[0].ljust(10), i[1].ljust(16), i[2].ljust(10), i[3]
+        if len(i)>4:
+            print str(ssh_info.index(i)).ljust(4), i[0].ljust(10), i[1].ljust(16), i[2].ljust(10), i[3].ljust(16), i[4]
+        else:
+            print str(ssh_info.index(i)).ljust(4), i[0].ljust(10), i[1].ljust(16), i[2].ljust(10), i[3]
 
 
-def select_nbr(ssh_info):
-    print_info(ssh_info)
-    nbr_input = raw_input('序号:')
-    if(nbr_input == 'q'):
+def select(ssh_infos):
+    input = raw_input('请输入序列号 or ip or hostname (q 退出):')
+    if(input == 'q'):
         exit(0)
-    if(nbr_input == ''):
-        if ssh_info is not None:
-            select_ip(ssh_info)
-    else:
-        nbr = int(nbr_input)
-        if(nbr > (len(ssh_info) - 1) or nbr < 0):
-            print '序号的范围只是0到%s,请重新选择' % (len(ssh_info) - 1)
-            select_nbr(ssh_info)
-        ssh_info_one = ssh_info[nbr]
+    try:
+        input = int(input)
+        if input <= len(ssh_infos) and input > 0:
+            ssh(ssh_infos[input])
+            return
+        else:
+            input = str(input)
+    except ValueError as verr:
+        print verr
 
-        user = ssh_info_one[0]
-        ip = ssh_info_one[1]
-        passwd = ssh_info_one[2]
-        #ssh(ssh_info_one[0], ssh_info_one[1], ssh_info_one[2])
-        return user, ip, passwd
-
-
-def select_ip(ssh_info):
-    ip_input = raw_input('ip(近似值):')
-    if(ip_input == 'q'):
-        exit(0)
-
-    ssh_info_ip = []
-    for i in ssh_info:
-        index = i[1].find(ip_input)
+    selected_ssh_infos = []
+    for i in ssh_infos:
+        print i[1]
+        index = i[1].find(input)
         if(index != -1):
-            ssh_info_ip.append(i)
-    if(len(ssh_info_ip) == 0):
+            selected_ssh_infos.append(i)
+    if(len(selected_ssh_infos) == 0):
         print '没找到和这个ip有近似的'
-        select_nbr(ssh_info)
-    elif(len(ssh_info_ip) == 1):
-        ssh_info_one = ssh_info_ip[0]
-        user = ssh_info_one[0]
-        ip = ssh_info_one[1]
-        passwd = ssh_info_one[2]
-        ssh(user, ip, passwd)
+        select(ssh_infos)
+    elif(len(selected_ssh_infos) == 1):  # 找到一个，直接登录
+        ssh(selected_ssh_infos[0])
+        return
     else:
-        return select_nbr(ssh_info_ip)
+        return select(selected_ssh_infos)  # 找到一堆，再过滤
 
 
 if __name__ == "__main__":
-    ssh_info = init_ssh_info()
-    print_info(ssh_info)
+    ssh_info = readConf()
+    printInfo(ssh_info)
     if ssh_info is not None:
-        user, ip, passwd = select_ip(ssh_info)
-        ssh(user, ip, passwd)
+        select(ssh_info)
