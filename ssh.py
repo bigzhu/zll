dd61301ec13e25f1066607a5c82da7bf168757a6
bigzhu <vermiliondun@gmail.com>
2024-02-21 16:50:37 +0800

    基本改造完成

--- ssh.py
+++ ssh.py
@@ -1,76 +1,60 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # encoding=utf-8
 # file_name=ssh.py
 
-# from __future__ import print_function  # 为了让 Python2 的 print() 不要答应成 tuple
+# from __future__ import print_function  # 为了让 Python2 的 print() 不要打印成 tuple
 import os
 import sys
+from bigzhu_py.file_z import csv_z
+from bigzhu_py.print_z import table_print
 
 
-def readConf():
+def read_hosts():
     """
     读取置文件
     """
-    ssh_info = []
-    with open(sys.path[0] + "/ssh.ini", "r") as f:
-        for i in f:
-            ssh_one_info = i.split()
-            if len(ssh_one_info) != 0:
-                ssh_info.append(tuple(i.split()))
-    return sorted(ssh_info, key=lambda by: by[1])
+    # ssh_info = []
+    header, rows = csv_z.read_csv(f"{sys.path[0]}/hosts.csv")
+    return header, rows
+
+
+# 添加 number 和打印
+def print_info(header: list, rows: list):
+    header.insert(0, "Number")
+    print_rows = [[i] + rows[i] for i in range(len(rows))]
+    table_print([header] + print_rows, "    ")
 
 
 def ssh(ssh_info):
+    print(ssh_info)
     user = ssh_info[0]
     ip = ssh_info[1]
-    port = 22
-    if len(ssh_info) > 4:
-        port = ssh_info[4]
+    port = ssh_info[3] if len(ssh_info) > 4 else 22
     print("ssh 登录 %s 中......" % ssh_info[1])
     command = "export TERM=xterm;ssh -p %s %s@%s" % (port, user, ip)
     os.system(command)
 
 
-def printInfo(ssh_info):
-    for i in ssh_info:
-        if len(i) > 4:
-            print(
-                str(ssh_info.index(i)).ljust(4),
-                i[0].ljust(10),
-                i[1].ljust(16),
-                i[2].ljust(10),
-                i[3].ljust(16),
-                i[4],
-            )
-        else:
-            print(
-                str(ssh_info.index(i)).ljust(4),
-                i[0].ljust(10),
-                i[1].ljust(16),
-                i[2].ljust(10),
-                i[3],
-            )
-
-
-def select(ssh_infos):
-    printInfo(ssh_infos)
+def select(header: list, rows: list):
+    print_info(header, rows)
     # 输入
     i_value = input("请输入序列号 or ip or hostname (q 退出):")
     if i_value == "q":
         exit(0)
     try:
+        # 尝试转为int, 看输入是否为编号
         i_value = int(i_value)
-        if i_value <= len(ssh_infos):
-            ssh(ssh_infos[i_value])
+        if i_value <= len(rows):
+            ssh(rows[i_value])
             return
         else:
             i_value = str(i_value)
-    except ValueError as verr:
+    except ValueError:
         pass
         # print verr
 
     selected_ssh_infos = []
-    for i in ssh_infos:
+    for i in rows:
         index = i[1].find(i_value)
         if index != -1:
             selected_ssh_infos.append(i)
@@ -81,16 +65,16 @@ def select(ssh_infos):
 
     if not selected_ssh_infos:
         print("没找到和这个ip有近似的")
-        select(ssh_infos)
+        select(header, rows)
     elif len(selected_ssh_infos) == 1:  # 找到一个，直接登录
         ssh(selected_ssh_infos[0])
         return
     else:
         print("找到%s个匹配%s的, 请再次选择!" % (len(selected_ssh_infos), i_value))
-        return select(selected_ssh_infos)  # 找到一堆，再过滤
+        return select(header, selected_ssh_infos)  # 找到一堆，再过滤
 
 
 if __name__ == "__main__":
-    ssh_info = readConf()
-    if ssh_info is not None:
-        select(ssh_info)
+    header, hosts = read_hosts()
+    if hosts is not None:
+        select(header, hosts)
